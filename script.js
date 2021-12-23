793808dfece6e3f160bc7302321c8f985099d0bf
imanmansour86 <imanmansour86@gmail.com>
2021-12-22 22:32:05 -0800

    adding promptuser function

--- script.js
+++ script.js
@@ -2,29 +2,51 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var letters = ["a", "b", "c"];
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var specialCharacters = "!$^&*-=+_?";
+
+  var password = "";
+
+  var passwordLength = promptUser();
+  console.log("here " + passwordLength);
+
+  // var lowercase = confirm("Do you want to include lowercase?");
+  // var uppercase = confirm("Do you want to include uppercase?");
+  // var numeric = confirm("Do you want to include numeric?");
+  // var special = confirm("Do you want to include special characters?");
+
+  // var generatedPaswword = "";
+
+  // for (var i = 0; i < passwordLength; i++) {
+  //   var test = Math.floor(Math.random() * passwordLength);
+  // }
+}
+
+function promptUser() {
   var passwordLength = prompt(
     "How many characters would you like your password to contain?",
     "Enter a number"
   );
-
+  //If user cancel the prompt
+  if (!passwordLength) {
+    return;
+  }
+  //check if the entered answered is a number
   passwordLength = parseInt(passwordLength);
-
   if (isNaN(passwordLength)) {
     alert("This is not a number, enter a number between 8 and 128");
+    promptUser();
   } else if (passwordLength < 8) {
     alert("Password length must be at least 8 characters");
   } else if (passwordLength > 128) {
     alert("Password length must be less than 129 characters");
   }
-
-  var lowercase = confirm("Do you want to include lowercase?");
-  var uppercase = confirm("Do you want to include uppercase?");
-  var numeric = confirm("Do you want to include numeric?");
-  var special = confirm("Do you want to include special characters?");
+  return passwordLength;
 }
 
-generatePassword();
+promptUser();
 
 // Write password to the #password input
 function writePassword() {
