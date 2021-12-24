e9678be0ca3290289079df1f213858e43d59547a
imanmansour86 <imanmansour86@gmail.com>
2021-12-23 21:17:36 -0800

    combining the code into one function

--- script.js
+++ script.js
@@ -2,28 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var password = "";
-  var characterSet = [];
+  var characterSet = "";
 
-  var passwordLength = promptUser();
-  console.log("passwordLength from user choice here is " + passwordLength);
-
-  var pw = "";
-  var text = "";
-  var testobject = "abcd123%$";
-  for (var i = 0; i < passwordLength; i++) {
-    // var test = Math.floor(Math.random() * passwordLength);
-    text = testobject.charAt(Math.floor(Math.random() * testobject.length));
-    pw = pw + text;
-    // pw += test;
-
-    // return pw;
-    //   console.log("my pw + " + pw);
-  }
-  console.log("my great pw is " + pw);
-}
-
-function promptUser() {
   var passwordLength = prompt(
     "How many characters would you like your password to contain?",
     "Enter a number"
@@ -36,48 +16,65 @@ function promptUser() {
   passwordLength = parseInt(passwordLength);
   if (isNaN(passwordLength)) {
     alert("This is not a number, enter a number between 8 and 128");
-    promptUser();
+    return;
   } else if (passwordLength < 8) {
     alert("Password length must be at least 8 characters");
   } else if (passwordLength > 128) {
     alert("Password length must be less than 129 characters");
   } else if (passwordLength >= 8 && passwordLength < 129) {
-    var lowercase = confirm("Do you want to include lowercase?");
-    var uppercase = confirm("Do you want to include uppercase?");
-    var numeric = confirm("Do you want to include numeric?");
-    var special = confirm("Do you want to include special characters?");
-    if (!lowercase && !uppercase && !numeric && !special) {
+    var lowercaseConfirm = confirm("Do you want to include lowercase?");
+    var uppercaseConfirm = confirm("Do you want to include uppercase?");
+    var numericConfirm = confirm("Do you want to include numeric?");
+    var specialConfirm = confirm("Do you want to include special characters?");
+    if (
+      !lowercaseConfirm &&
+      !uppercaseConfirm &&
+      !numericConfirm &&
+      !specialConfirm
+    ) {
       alert("At least one character type should be selected");
     }
 
     var lowercase = "abcdefghijklmnopqrstuvwxyz";
     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numbers = "0123456789";
-    var specialCharacters = "!$^&*-=+_?";
+    var specialCharacters = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
     characterSet = "";
-    if (lowercase) {
-      characterSet = lowercase;
-    } else if (lowercase && uppercase) {
-      characterSet = lowercase + uppercase;
-    } else if (lowercase && specialCharacters) {
-      characterSet = lowercase + specialCharacters;
-    } else if (lowercase && uppercase && specialCharacters) {
-      characterSet = lowercase + uppercase + specialCharacters;
-    } else if (uppercase) {
-      characterSet = uppercase;
-    } else if (uppercase && specialCharacters) {
-      characterSet = uppercase + specialCharacters;
-    } else if (specialCharacters) {
-      characterSet = specialCharacters;
+
+    if (lowercaseConfirm) {
+      characterSet += lowercase;
+    }
+    if (uppercaseConfirm) {
+      characterSet += uppercase;
+    }
+    if (specialConfirm) {
+      characterSet += specialCharacters;
+    }
+    if (numericConfirm) {
+      characterSet += numbers;
     }
 
     console.log("character set here is " + characterSet);
   }
 
+  var pw = "";
+  var text = "";
+
+  for (var i = 0; i < passwordLength; i++) {
+    // var test = Math.floor(Math.random() * passwordLength);
+    text = characterSet.charAt(Math.floor(Math.random() * characterSet.length));
+    pw = pw + text;
+    // pw += test;
+
+    // return pw;
+    //   console.log("my pw + " + pw);
+  }
+  console.log("my great pw is " + pw);
+
   //condition for special chatacter
 
-  return passwordLength; //return number of characters for the password
+  return pw; //return number of characters for the password
 }
 
 // Write password to the #password input
